c2bccfb7354931f1d9f4a4a45d4e9a720cab4003
Shaked-Dvir <shaked620d@gmail.com>
2026-04-22 16:23:12 +0300

    Adding function which is responsible on task managmen by location

--- script/data.js
+++ script/data.js
@@ -5,32 +5,49 @@ export const textsForlocations = [
         text: "החתלתול הקטן הלך לאיבוד. עזרו לנו למצוא אותו!",
         src:'../images/garden.png',
         buttensSrc: ["../images/r1.png","../images/r2.png"],
-        mission: false
+        missionItems: [
+    {
+        question: "כמה חיפושיות יש בגינה?",
+        correctAnswer: "5",
+    },
+    {
+        question: "כמה פטריות יש בגינה?",
+        correctAnswer: "4",
+    },
+    {
+        question: "כמה חלונות יש לבית?",
+        correctAnswer: "6",
+    },
+    {
+        question: "כמה חמניות יש בגינה?",
+        correctAnswer: "2",
+    }
+]
     },
     {
         id: 2,
         text: "בצעו את המשימה והיכנסו לחדר הבא",
         src:'../images/livingRoom.png',
-        buttens: [],
-        mission: false
+        buttensSrc: [],
+        missionItems:[]
     },
     {
         id: 3,
         text: "הקשב!!! יש יללות חתול! המשימה עכשיו למצוא את החתלתול!",
         src:'../images/kitchen.png',
-        buttens: [],
-        mission: false
+        buttensSrc: [],
+        missionItems:[]
     }
 ];
 
 let timer;
-let loc =0;
+let loc =1;
 /*פונקצייה שמקבלת מיקום דף מציגה את מראה הדף ומקלידה את הטקסט של הדף בזמן אמת ע"י פונקציית טייפ פנימית */
 export function showLocation(currentId) {
  
     const room = document.getElementById('room');
     // ניקוי החדר לפני הוספת תוכן חדש
-
+    room.replaceChildren();
     const image = document.createElement('img');
     image.className = 'mainImg';
     image.src = textsForlocations[currentId-1].src;
@@ -62,7 +79,7 @@ function  showOptions()
 {   const options = document.getElementById('options');
     options.replaceChildren(); // ניקוי כפתורים קודמים
 
-    const currentData = textsForlocations[loc];
+    const currentData = textsForlocations[loc-1];
     
     // בדיקה אם קיימים כפתורים להצגה 
     const sources = currentData.buttensSrc || [];
@@ -70,7 +87,7 @@ function  showOptions()
    sources.forEach((src, i) => {
         let btnImg = document.createElement('img');
         btnImg.src = '../images/dest.png'; // התמונה הקטנה שעליה לוחצים
-        btnImg.id = `btn_${loc}_${i+1}`;
+        btnImg.id = `btn_${loc-1}_${i+1}`;
         btnImg.className = 'btn-click'; 
         options.appendChild(btnImg);
 
@@ -91,6 +108,35 @@ function  showOptions()
 
 };
     });
-
+taskManagement();
 } 
+
+function taskManagement()
+ {
+    switch(loc-1)
+    {
+       case 0:
+            const opt = document.getElementById('options');
+
+             // יצירת הכפתור שיוצג באופשנס
+             const playMission = document.createElement('button');
+             playMission.textContent = 'התחל משימת שאלון';
+             playMission.className = 'mission1-btn';
+
+             // כשלוחצים על הכפתור - הטופס נוצר
+             playMission.onclick = () => {
+              playMission.remove();
+              mission1_quiz(); // קריאה לפונקציה שבונה את השאלות
+             };
+             opt.appendChild(playMission);
+             break;
+
+       case 1:
+        mission2_collect();
+        break;
+       case 2:
+            mission3_drag();
+            break;
+    }
+ }
 
