7f83831d4c947f070642dbc04403c2a949db041d
Shaked-Dvir <shaked620d@gmail.com>
2026-05-05 15:49:44 +0300

    Adding music!

--- script/main_garden.js
+++ script/main_garden.js
@@ -2,6 +2,18 @@ import { textsForlocations,showLocation } from './data.js';
 
 let currentLocationId;
 let start = false;
+// בתוך קובץ ה-JS של דף המשחק
+window.addEventListener('DOMContentLoaded', (event) => {
+    const backgroundMusic = new Audio('../sound/sound.mp3');
+    //המנגינה תחזור על עצמה בלופים
+    backgroundMusic.loop = true;
+
+    backgroundMusic.play().catch(error => {
+        console.log("השמעה אוטומטית נחסמה, ננסה להפעיל בלחיצה ראשונה על המסך");
+        // פתרון גיבוי למקרה שהדפדפן חסם:
+        document.body.onclick = () => backgroundMusic.play();
+    });
+});
 
 /*פונקציית התחלת המשחק מאפסת את המשתנים וקוראת לפונקציית טעינת מראה המיקום */
  function startGame() {
